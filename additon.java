0605a35ec2847953dec86f41bc1106503fcde705
Your Name <you@example.com>
2022-07-13 21:18:15 +0530

    add & mul is done

--- additon.java
+++ additon.java
@@ -2,9 +2,16 @@ class New
 {
 public static void main(String[] args)
 {
+//Addition
 int a=10;
 int b=5;
-int c=a*b;
+int c=a+b;
 System.out.println(c);
+
+//multiplication
+int i=10;
+int j=5;
+int d=i*j;
+System.out.println(d);
 } 
 }
